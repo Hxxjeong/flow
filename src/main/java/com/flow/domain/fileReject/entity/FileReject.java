3c356b47bc446a50eac2cc06d333d23e374d8a8e
Hxxjeong <heejeong3394@gmail.com>
2025-06-21 17:55:36 +0900

    Fix: Entity PK 설정 변경

--- src/main/java/com/flow/domain/fileReject/entity/FileReject.java
+++ src/main/java/com/flow/domain/fileReject/entity/FileReject.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FileReject {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "extension", unique = true, nullable = false)
